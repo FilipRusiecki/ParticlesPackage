c6825afbee35b61da8050de2a246f9cb685b2d4d
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-11-14 10:37:10 +0000

    Made casings customizable

--- Particles/Assets/UseTheseScripts/PutOnPlayer/CasingSpawn.cs
+++ Particles/Assets/UseTheseScripts/PutOnPlayer/CasingSpawn.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CasingSpawn : MonoBehaviour
 {
+    [Header("Change force here")]
+    public float Force;
+    [Header("How many times to spawn")]
+    public float amountOfBullets;
     [Header("Add Bullet Casing Here")]
     public GameObject casingObject;
     [Header("Add Transform where you want to spawn it")]
@@ -11,11 +15,14 @@ public class CasingSpawn : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0))  //<-------- change to any key that you would like eg: "KeyCode.AnyKeyYouWouldLike"
+        if(Input.GetKeyDown(KeyCode.Mouse0))  //<-------- change to any key that you would like eg: "KeyCode.AnyKeyYouWouldLike"
         {
-            GameObject castingBullet = Instantiate(casingObject, transformm.position, transformm.rotation);
-            Rigidbody2D rbCasting = castingBullet.GetComponent<Rigidbody2D>();
-            rbCasting.AddForce(transformm.up * 5, ForceMode2D.Impulse);
+            for (int i = 0; i < amountOfBullets; i++)
+            {
+                GameObject castingBullet = Instantiate(casingObject, transformm.position, transformm.rotation);
+                Rigidbody2D rbCasting = castingBullet.GetComponent<Rigidbody2D>();
+                rbCasting.AddForce(transformm.up * Force, ForceMode2D.Impulse);
+            }
         } 
     }
 }
